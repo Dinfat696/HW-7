005cd3953907ae32bc20a8f91802c1107b7f107c
dinarfathrahmanov <dinfat@mail.ru>
2023-02-15 22:38:45 +0300

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        /*task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();*/
     }
-}
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int salary = 15000;
+        int total = 0;
+        int i = 0;
+        while (total <= 2459000) {
+            i++;
+            total = total + salary;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int start = 1;
+        while (start <= 10) {
+            System.out.print(start + " ");
+            start = start + 1;
+        }
+            System.out.println();
+            for (int i = 10; i > 0; i--) {
+                System.out.print(i + " ");
+            }
+        }
+    }
