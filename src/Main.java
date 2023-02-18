4fc5093877754c21358b6696c456b87256fb576e
dinarfathrahmanov <dinfat@mail.ru>
2023-02-18 11:37:31 +0300

    Исправил №3 задачу

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,7 @@ public class Main {
         task6();
         task7();
         task8();
-        /*task9();
-        task10();*/
+
     }
 
     public static void task1() {
@@ -41,23 +40,23 @@ public class Main {
     public static void task3() {
         System.out.println("\nЗадача 3");
         int people = 12000000;
-        int newBorn = people / 1000 * 17;
-        int die = people / 1000 * 8;
-        int year = 0;
-        while (year < 10) {
+        int year=0;
+        for (int i=0;i<10;i++) {
             year++;
+            int newBorn = people / 1000 * 17;
+            int die = people / 1000 * 8;
             people = people + newBorn - die;
-            System.out.println("Год " + year + " численность населения составляет " + people);
+            System.out.printf("\nГод " + year + " численность населения составляет " +people);
         }
     }
 
     public static void task4() {
-        System.out.println("Задача 4");
+        System.out.println("\nЗадача 4");
         int total = 15000;
         int i = 0;
         while (total < 12000000) {
             i++;
-            total = total + total / 100 * 7;
+            total = total+total/100*7;
             System.out.println("Месяц " + i + " сумма накоплений равна " + total + "рублей");
         }
     }
